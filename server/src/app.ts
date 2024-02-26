d98b83e5090307ff6a089cde2af749bb938116a3
AdilArslann <adilarslan3@gmail.com>
2024-02-26 16:55:27 +0200

    Deleted and reinstalled node_modules and package-lock.json

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,23 +15,25 @@ import config from './config'
 export default function createApp(db: Database) {
   const app = express()
 
-  Sentry.init({
-    dsn: config.sentryDSN,
-    integrations: [
-      // enable HTTP calls tracing
-      new Sentry.Integrations.Http({ tracing: true }),
-      // enable Express.js middleware tracing
-      new Sentry.Integrations.Express({ app }),
-      new ProfilingIntegration(),
-    ],
-    // Performance Monitoring
-    tracesSampleRate: 1.0, //  Capture 100% of the transactions
-    // Set sampling rate for profiling - this is relative to tracesSampleRate
-    profilesSampleRate: 1.0,
-  })
-  app.use(Sentry.Handlers.requestHandler())
-  app.use(Sentry.Handlers.tracingHandler())
-  app.use(Sentry.Handlers.errorHandler())
+  if (config.sentryDSN) {
+    Sentry.init({
+      dsn: config.sentryDSN,
+      integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Sentry.Integrations.Express({ app }),
+        new ProfilingIntegration(),
+      ],
+      // Performance Monitoring
+      tracesSampleRate: 1.0, //  Capture 100% of the transactions
+      // Set sampling rate for profiling - this is relative to tracesSampleRate
+      profilesSampleRate: 1.0,
+    })
+    app.use(Sentry.Handlers.requestHandler())
+    app.use(Sentry.Handlers.tracingHandler())
+    app.use(Sentry.Handlers.errorHandler())
+  }
 
   app.use(cors())
   app.use(express.json())
