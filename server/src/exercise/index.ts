0c569a893aa05b77b697667f988f4ba6449fcb71
Adil <82507565+AdilArslann@users.noreply.github.com>
2024-04-07 16:53:57 +0300

    Solution with higher order functions index.ts

--- server/src/exercise/index.ts
+++ server/src/exercise/index.ts
@@ -3,40 +3,15 @@
 // each dates are the checkoutdates
 // first 5 days 25 cents any additional days are 50 cents
 
-export function feeCalculator(returnDate: Date, checkoutdates: Date[]) {
-  let fee = 0.0
-  const [year, month, day] = dateToNumbers(returnDate)
-
-  checkoutdates.forEach((datee) => {
-    let difference = 0
-
-    const [yearr, monthh, dayy] = dateToNumbers(datee)
-    if (+year > +yearr) {
-      const temp = +year - +yearr
-      difference += temp * 365
-    }
-    if (+month > +monthh) {
-      const temp = +month - +monthh
-      difference += temp * 30
-    }
-
-    difference += +day - +dayy
-
-    let i: number = 0
-
-    while (i < difference) {
-      if (i < 5) {
-        fee += 0.25
-      } else {
-        fee += 0.5
-      }
-      i += 1
-    }
-  })
-  return fee
-}
-
-function dateToNumbers(dateToBeModified: Date) {
-  const [returnsDate] = dateToBeModified.toISOString().split('T')
-  return returnsDate.split('-')
+export function feeCalculator(returnDate: Date, checkoutdates: Date[]): number {
+  return checkoutdates
+    .map((checkoutdate) => {
+      const differenceMs = returnDate.getTime() - checkoutdate.getTime()
+      const differenceDays = differenceMs / (1000 * 60 * 60 * 24)
+      const fee =
+        Math.min(5, differenceDays) * 0.25 +
+        Math.max(0, differenceDays - 5) * 0.5
+      return fee
+    })
+    .reduce((accumulated, current) => (accumulated += current))
 }
